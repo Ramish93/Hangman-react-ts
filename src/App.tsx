94f1137631370508917f71358f419ed868609b90
Ramish Hassan <ramishshah93@gmail.com>
2023-01-10 14:59:58 +0100

    basic component structure done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
 import { useState } from "react";
+import HangmanDrawing from "./components/HangmanDrawing";
+import Keyboard from "./components/Keyboard";
+import HangmanWord from "./HangmanWord";
+import words from "./wordList.json";
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState("test");
-  return <h1>Hi</h1>;
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "30px",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>lose win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 }
 
 export default App;
