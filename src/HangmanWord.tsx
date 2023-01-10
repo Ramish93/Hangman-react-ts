843bb3595684240a0f23f8a0b2584d3881fc4ef5
Ramish Hassan <ramishshah93@gmail.com>
2023-01-10 20:55:01 +0100

    words to guess done as design in hangmanWord component

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+const word = "test";
+const guessedLetters = ["e", "t"];
 
 const HangmanWord = () => {
-  return <div>HangmanWord</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default HangmanWord;
