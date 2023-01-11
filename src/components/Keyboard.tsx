b7b6c7f74dd061ae5343025c2901f95d9a48c404
Ramish Hassan <ramishshah93@gmail.com>
2023-01-11 12:24:51 +0100

    Keyboard design complete, implimentation 30% done, moved hangmanword comp to its folder

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+const KEYS = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
 const Keyboard = () => {
-  return <div>Keyboard</div>;
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+        gap: "0.5rem",
+      }}
+    >
+      {KEYS.map((key) => (
+        <button key={key}>{key}</button>
+      ))}
+    </div>
+  );
 };
 
 export default Keyboard;
